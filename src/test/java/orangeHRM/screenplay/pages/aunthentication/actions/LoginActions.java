f8bb39c5c36c5c763a140becb920b4f1a13dca18
Kelly Zapata <kelly_zapata27@hotmail.com>
2023-02-27 19:13:43 -0500

    Login test implemented OK using serenityBDD

---
+++ src/test/java/orangeHRM/screenplay/pages/aunthentication/actions/LoginActions.java
@@ -0,0 +1,15 @@
+package orangeHRM.screenplay.pages.aunthentication.actions;
+
+import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.thucydides.core.annotations.Step;
+
+public class LoginActions extends UIInteractionSteps {
+
+    @Step("Log in as {0} user")
+    public void as(User user) {
+        openUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        $("[name=username]").sendKeys(user.getUsername());
+        $("[name=password]").sendKeys(user.getPassword());
+        $("//button[contains(.,'Login')]").click();
+    }
+}
